7e6582093bf688ec6526cfa6abb50d4be8774142
maria <masha.gul15@gmail.com>
2020-03-30 20:25:31 +0300

    heroku

--- src/project/urls.py
+++ src/project/urls.py
@@ -7,7 +7,41 @@ def view(r):
     index = here.parent.parent / "index.html"
     with index.open() as f:
         return HttpResponse(f.read())
+def resume(r):
+    index = here.parent.parent / "Resume.html"
+    with index.open() as f:
+        return HttpResponse(f.read())
+def thoughts(r):
+    index = here.parent.parent / "Thoughts.html"
+    with index.open() as f:
+        return HttpResponse(f.read())
+def img(rb):
+    index=here.parent.parent / "gBAs.jpg"
+    with index.open("rb") as f:
+        return  HttpResponse(f.read())
+def bootstrap(r):
+    index=here.parent.parent / "css/bootstrap.min.css"
+    with index.open() as f:
+        return HttpResponse(f.read())
+def hero(r):
+    index=here.parent.parent / "css/hero-slider-style.css"
+    with index.open() as f:
+        return HttpResponse(f.read())
+def magn(r):
+    index=here.parent.parent / "css/magnific-popup.css"
+    with index.open() as f:
+        return HttpResponse(f.read())
+def toop(r):
+    index=here.parent.parent / "css/tooplate-style.css"
+    with index.open() as f:
+        return HttpResponse(f.read())
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view),
+    path('Resume.html',resume),
+    path('Thoughts.html', thoughts),
+    path('bootstrap.css', bootstrap),
+    path('hero-slider-style.css', hero),
+    path('magnific-popup.css', magn),
+    path('tooplate-style.css', toop)
 ]
