217d752c07e82ad68bc1869832d66abd151e53cc
maria <masha.gul15@gmail.com>
2020-04-03 17:02:41 +0300

    findecorator

--- src/project/urls.py
+++ src/project/urls.py
@@ -3,53 +3,54 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 here = Path(__file__).parent.resolve()
-def glob_view(file="Resume.html", type="text/html"):
+def static_re(file="Resume.html", type="text/html"):
     index=here.parent.parent / file
     with index.open("rb") as f:
         return HttpResponse(f.read(),content_type=type)
 def view(r):
-    return glob_view(file="index.html")
+    return static_re(file="index.html")
 
 def resume(r):
-    return glob_view()
+    return static_re()
 
 def thoughts(r):
-    return glob_view("Thoughts.html")
+    #print(r.Get())
+    return static_re("Thoughts.html")
 
 def img(rb):
-    return glob_view("gBAs.jpg","image/jpg")
+    return static_re("gBAs.jpg","image/jpg")
 
 def ico(r):
-    return glob_view("favicon.ico", "image/x-icon")
+    return static_re("favicon.ico", "image/x-icon")
 
 def bootstrap(r):
-    return glob_view("css/bootstrap.min.css","text/css")
+    return static_re("css/bootstrap.min.css","text/css")
 
 def hero(r):
-    return glob_view("css/hero-slider-style.css", "text/css")
+    return static_re("css/hero-slider-style.css", "text/css")
 
 def magn(r):
-    return glob_view("css/magnific-popup.css", "text/css")
+    return static_re("css/magnific-popup.css", "text/css")
 
 def toop(r):
-    return glob_view("css/tooplate-style.css", "text/css")
+    return static_re("css/tooplate-style.css", "text/css")
 
 def awesome(r):
-    return glob_view("css/font-awesome.min.css", "text/css")
+    return static_re("css/font-awesome.min.css", "text/css")
 
 def woff2(r):
-    return glob_view("fonts/fontawesome-webfont.woff2","font/woff2")
+    return static_re("fonts/fontawesome-webfont.woff2","font/woff2")
 
 def ttf(r):
-    return glob_view("fonts/fontawesome-webfont.ttf","font/ttf")
+    return static_re("fonts/fontawesome-webfont.ttf","font/ttf")
 
 def woff(r):
-    return glob_view("fonts/fontawesome-webfont.woff","font/woff")
+    return static_re("fonts/fontawesome-webfont.woff","font/woff")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view),
-    path('Resume.html',resume),
+    path('Resume.html', resume),
     path('Thoughts.html',thoughts),
     path('index.html', view),
     path('css/bootstrap.min.css', bootstrap),
